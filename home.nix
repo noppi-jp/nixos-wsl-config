55a545127c5bb7ba43fc1878437dc6659cac16d9
Noppi <noppi@noppi.jp>
2025-07-23 14:21:44 +0900

    コード整理

--- home.nix
+++ home.nix
@@ -47,21 +47,7 @@
 
     neovim = {
       enable = true;
-      extraConfig = ''
-        colorscheme vim
-        set expandtab
-        set ignorecase
-        set number
-        set shiftwidth=0
-        set showmatch
-        set smartcase
-        set smartindent
-        set tabstop=4
-        set termguicolors
-
-        set ambiwidth=double
-        set fileencodings=ucs-bom,utf-8,iso-2022-jp,sjis,euc-jp,cp932,utf-16le,default,laten1
-      '';
+      extraConfig = builtins.readFile ./home/.config/nvim/init.vim;
     };
 
     # vscode = {
